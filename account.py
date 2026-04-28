53ca3066e0251ec863e934af520c3fd7aa8c3d20
Simon1130 <3075302l@student.gla.ac.uk>
2026-04-28 20:56:56 +0100

    defining class and some attributes

---
+++ account.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 20:53:12 2026
+
+@author: simon
+"""
+
+class Account:
+    def __init__(self, username, password, binary_balance, is_locked, num_tries):
+        self.username = username
+        self.password = password
+        self.binary_balance = binary_balance
+        self.is_locked = is_locked
+        self.num_tries = num_tries
+        
+    def bin_to_dec():
+        
+    def dec_to_bin:
+        
+    def check_balance(self):
+        
+    def deposit_money(self, money):
+        
+    def withdraw_money(self, money):
+        
---
+++ bank_system.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 20:54:30 2026
+
+@author: simon
+"""
+
+class Bank:
+    def __init__(self, user_list):
+        self.user_list = user_list
+        
+    def create_account(self):
+        
+    def transfer_money(self):
+        
